bae743e7dfffe6b8576f588a7a7125922f412476
Lajja123 <lajja.lamprostech@gmail.com>
2024-02-02 17:08:34 +0530

    update homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,39 @@
 import homeStyle from "./styles/home.module.scss";
 import HomeSection from "./components/home/homeSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
       <div className={homeStyle.lmplabHomepageMainBg}>
+        <Head>
+          <title>Home Page</title>
+          <meta name="description" content="Home Page Description..." />
+          <meta name="keywords" content="react, meta tags, seo" />
+          <meta name="author" content="Your Name" />
+          <meta property="og:title" content="Home Page" />
+          <meta property="og:description" content="Home Page Description..." />
+          <meta
+            property="og:image"
+            content="https://lampros-labs-nextjs-new.vercel.app/"
+          />
+          <meta
+            property="og:url"
+            content="https://lampros-labs-nextjs-new.vercel.app/"
+          />
+          <meta name="twitter:title" content="My Page Title" />
+          <meta
+            name="twitter:description"
+            content="This is a description of my page"
+          />
+          <meta
+            name="twitter:image"
+            content="https://lampros-labs-nextjs-new.vercel.app/"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
         <HomeSection />
       </div>
     </div>
   );
 }
-export const metadata = {
-  title: "Home Page",
-  description: "Home Page Description...",
-  openGraph: {
-    title: "Home Page",
-    description: "Home Page Description...",
-    url: "https://lampros-labs-nextjs-new.vercel.app/dao-roadmap",
-    type: "website",
-    siteName: "Next.js",
-    images: [
-      {
-        url: "https://www.kresko.fi/apple-touch-icon.png",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://www.kresko.fi/apple-touch-icon.png",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-    locale: "en_US",
-  },
-};
