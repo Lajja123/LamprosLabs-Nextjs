fd79d306e450a969f71e0825f7c295e557e8e5be
Lajja123 <lajja.lamprostech@gmail.com>
2024-02-02 14:44:53 +0530

    add meta-tags in all pages

---
+++ src/app/components/dao/daoSection.js
@@ -0,0 +1,295 @@
+"use client";
+import { useRef, useEffect, useState } from "react";
+import daoimg1 from "../../assets/images/daoimg1.webp";
+import daoimg2 from "../../assets/images/daoimg2.png";
+import daoimg3 from "../../assets/images/daoimg3.png";
+import daoimg4 from "../../assets/images/daoimg4.png";
+import daoimg5 from "../../assets/images/daoimg5.png";
+import daoimg6 from "../../assets/images/daoimg6.png";
+import daoimg7 from "../../assets/images/daoimg7.png";
+import daoimg8 from "../../assets/images/daoimg8.png";
+import daoFooterImg from "../../assets/images/dao-page-footer-img.png";
+import daoline from "../../assets/images/daoline.png";
+import { Fade } from "react-reveal";
+import daoStyle from "../../styles/dao.module.scss";
+import Image from "next/image";
+import ScrollBtn from "../scrollbarBtn";
+
+function daoSection() {
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const newOpacity = 1 - scrollPosition / 1000; // Adjust 500 based on when you want the opacity change to occur
+
+      if (newOpacity >= 0) {
+        setOpacity(newOpacity);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className={daoStyle.daomainbg}>
+      <ScrollBtn
+        section1Ref={section1Ref}
+        section2Ref={section2Ref}
+        section3Ref={section3Ref}
+      />
+      <div className="dao-roadmap">
+        <div
+          className={`${daoStyle["daosec1main"]} d-none d-md-flex`}
+          ref={section1Ref}
+          smooth={true}
+          duration={600}
+        >
+          <Fade bottom duration={1000} distance="20px">
+            <div className={daoStyle.daosec1flex1}>
+              <div className={daoStyle.daoroadmap2}>DAO Roadmap</div>
+              <div className={daoStyle.weEnvisionA}>
+                We envision a future where Lampros Labs transforms into a
+                decentralized autonomous organization (DAO), a nexus where
+                collective intelligence drives innovation and growth. Our
+                roadmap sketches the pathway to this transformation,
+                highlighting the pivotal milestones that will shape our
+                decentralized future. Here, we outline the structured phases
+                that will lead us to become a more collaborative, autonomous,
+                and community-driven entity.
+              </div>
+            </div>
+          </Fade>
+
+          <div className={daoStyle.daosec1flex2}>
+            <Image
+              className={daoStyle.illustration141}
+              alt=""
+              src={daoimg1}
+              style={{ opacity }}
+            />
+            <Image
+              className={daoStyle.mobileImg7}
+              alt=""
+              src={daoimg7}
+              style={{ opacity }}
+            />
+          </div>
+        </div>
+
+        <Fade bottom duration={1000} distance="20px">
+          <div
+            className={`${daoStyle["lamproslabsroadmapcontainer"]} for-laptops d-none d-md-block`}
+          >
+            <div ref={section2Ref} smooth={true} duration={200}>
+              Lampros Labs <span className={daoStyle.roadmap}>Roadmap</span>
+            </div>
+            <div>to Becoming a DAO</div>
+          </div>
+        </Fade>
+        <div className="d-md-none">
+          <div className={daoStyle.daosec1flex1}>
+            <Fade bottom duration={1000} distance="20px">
+              <div className={daoStyle.daoroadmap2}>DAO Roadmap</div>
+              <div className={daoStyle.weEnvisionA}>
+                We envision a future where Lampros Labs transforms into a
+                decentralized autonomous organization (DAO), a nexus where
+                collective intelligence drives innovation and growth. Our
+                roadmap sketches the pathway to this transformation,
+                highlighting the pivotal milestones that will shape our
+                decentralized future. Here, we outline the structured phases
+                that will lead us to become a more collaborative, autonomous,
+                and community-driven entity.
+              </div>
+            </Fade>
+            <Image className={daoStyle.mobiledaoImg} alt="" src={daoimg7} />
+          </div>
+        </div>
+
+        <div className="for-mobiles d-md-none">
+          <Fade bottom duration={1000} distance="50px">
+            <div className={daoStyle.lamproslabsroadmapcontainer}>
+              <div>
+                Lampros Labs <br />
+                <span className={daoStyle.roadmap}>Roadmap</span> TO <br />
+                Becoming a dao
+              </div>
+              <div className="d-md-none">
+                <Image
+                  className={daoStyle.mobileheadingleftImage}
+                  alt=""
+                  src={daoimg8}
+                />
+              </div>
+            </div>
+          </Fade>
+        </div>
+
+        <div
+          className={daoStyle.daosec2main}
+          ref={section3Ref}
+          smooth={true}
+          duration={600}
+        >
+          <Image className={daoStyle.daoroadmapInner} alt="" src={daoline} />
+          <div className="dao-sec2-first">
+            <Image className="design311" alt="" src={daoimg2} />
+            <Image
+              className="design-32-1 d-none d-md-block"
+              alt=""
+              src={daoimg3}
+            />
+
+            <div
+              className={`${daoStyle["daobox1main"]} ${daoStyle["daoboxstyleleft"]} `}
+            >
+              <div className={daoStyle.daoboxheading}>Foundation Phase</div>
+              <div>
+                <ul
+                  className={`${daoStyle["daoboxpera"]} ${daoStyle["customlist"]} `}
+                >
+                  <li className={daoStyle.daoLi}>DAO Concept Introduction</li>
+                  <li className={daoStyle.daoLi}>Community Brainstorming</li>
+                  <li className={daoStyle.daoLi}>Feasibility Analysis</li>
+                </ul>
+              </div>
+            </div>
+
+            <div
+              className={`${daoStyle["daobox2main"]} ${daoStyle["daoboxstyleleft"]} `}
+            >
+              <div className={daoStyle.daoboxheading}>
+                Development & Design Phase
+              </div>
+              <div>
+                <ul
+                  className={`${daoStyle["daoboxpera"]} ${daoStyle["customlist"]} `}
+                >
+                  <li className={daoStyle.daoLi}>DAO Framework Formulation</li>
+                  <li className={daoStyle.daoLi}>
+                    Technical Architecture Design
+                  </li>
+                  <li className={daoStyle.daoLi}>Pilot DAO Launch</li>
+                </ul>
+              </div>
+            </div>
+
+            <div
+              className={`${daoStyle["daobox3main"]} ${daoStyle["customlist"]} ${daoStyle["daoboxstyleleft"]} `}
+            >
+              <div className={daoStyle.daoboxheading}>Implementation Phase</div>
+              <div>
+                <ul
+                  className={`${daoStyle["daoboxpera"]} ${daoStyle["customlist"]} `}
+                >
+                  <li className={daoStyle.daoLi}>Governance Token Issuance</li>
+                  <li className={daoStyle.daoLi}>Infrastructure Integration</li>
+                  <li className={daoStyle.daoLi}>Community Education</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="dao-sec2-second">
+            <div
+              className={`${daoStyle["daobox4main"]} ${daoStyle["daoboxstyleright"]} `}
+            >
+              <div className={daoStyle.daoboxheading}>
+                Iteration & Enhancement Phase
+              </div>
+              <div>
+                <ul className={daoStyle.daoboxpera1}>
+                  <li className={daoStyle.daoLi}>
+                    Community Feedback and Adjustments
+                  </li>
+                  <li className={daoStyle.daoLi}>
+                    Continuous Learning Modules
+                  </li>
+                  <li className={daoStyle.daoLi}>
+                    Proposal Mechanisms Initiated
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div
+              className={`${daoStyle["daobox5main"]} ${daoStyle["daoboxstyleright"]} `}
+            >
+              <div className={daoStyle.daoboxheading}>
+                Full Decentralization Phase
+              </div>
+              <div>
+                <ul className={daoStyle.daoboxpera1}>
+                  <li className={daoStyle.daoLi}>
+                    Complete Operational Transition to DAO
+                  </li>
+                  <li className={daoStyle.daoLi}>
+                    Decentralized Revenue Distribution
+                  </li>
+                  <li className={daoStyle.daoLi}>
+                    Community-Led Growth and Innovation
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div
+              className={`${daoStyle["daobox6main"]} ${daoStyle["daoboxstyleright"]} `}
+            >
+              <div className={daoStyle.daoboxheading}>
+                Review & Future Prospects Phase
+              </div>
+              <div>
+                <ul className={daoStyle.daoboxpera1}>
+                  <li className={daoStyle.daoLi}>
+                    Periodic Operational Review
+                  </li>
+                  <li className={daoStyle.daoLi}>Future Goal Outlining</li>
+                  <li className={daoStyle.daoLi}>
+                    Staying Ahead in the Web3 Evolution
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div>
+              <Image
+                className={`${daoStyle["daorightImg1"]}  d-none d-md-block`}
+                alt=""
+                src={daoimg4}
+              />
+            </div>
+            <div>
+              <Image
+                className={`${daoStyle["daorightImg2"]}  d-none d-md-block`}
+                alt=""
+                src={daoimg5}
+              />
+            </div>
+            <div>
+              <Image
+                className={`${daoStyle["daorightImg3"]}  d-none d-md-block`}
+                alt=""
+                src={daoimg6}
+              />
+            </div>
+            <div className="d-md-none">
+              <Image
+                className={daoStyle.daofooterImg}
+                alt=""
+                src={daoFooterImg}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default daoSection;
---
+++ src/app/components/hackathon/hackathonSection.js
@@ -0,0 +1,206 @@
+"use client";
+import sec1bg from "../../assets/images/hackimg.webp";
+import arrow from "../../assets/images/right-arrow.png";
+import { useRef } from "react";
+import { Fade } from "react-reveal";
+import Image from "next/image";
+import hackStyle from "../../styles/hackathon.module.scss";
+import ScrollBtn from "@/app/components/scrollbarBtn";
+
+function hackathonSection() {
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+  return (
+    <div>
+      <div className={hackStyle.inherimainbg}>
+        <ScrollBtn
+          section1Ref={section1Ref}
+          section2Ref={section2Ref}
+          section3Ref={section3Ref}
+        />
+        <div
+          className={hackStyle.hacksection1}
+          ref={section1Ref}
+          smooth={true}
+          duration={600}
+        >
+          <div className={`${hackStyle["hacksec1bg"]}  col-8`}>
+            <Image src={sec1bg}></Image>
+          </div>
+
+          <Fade duration={1500} delay={100}>
+            <div className={`${hackStyle["sec1box"]} col-xl-9 col-8 col-sm-9`}>
+              <div className={hackStyle.sec1boxtitle}>
+                Unleash Your Blockchain Potential
+              </div>
+              <div className={hackStyle.sec1boxpera}>
+                Dive into the realm of blockchain innovation with Lampros Labs’
+                exclusive hackathons! These events are meticulously crafted to
+                offer participants a platform to ideate, innovate, and showcase
+                their groundbreaking solutions. Whether you're a seasoned
+                developer or a Web3 newbie, our hackathons are the perfect stage
+                to challenge yourself and push boundaries.
+              </div>
+            </div>
+          </Fade>
+          <div
+            className={`${hackStyle["sec1boxmlflex"]} col-xl-9 col-10 col-sm-9`}
+          >
+            <div className={`${hackStyle["sec1boxtitle"]} d-sm-none`}>
+              Unleash Your Blockchain Potential
+            </div>
+            <div
+              className={`${hackStyle["sec1boxml"]} ${hackStyle["sec1boxpera"]}`}
+            >
+              Dive into the realm of blockchain innovation with Lampros Labs’
+              exclusive hackathons! These events are meticulously crafted to
+              offer participants a platform to ideate, innovate, and showcase
+              their groundbreaking solutions. Whether you're a seasoned
+              developer or a Web3 newbie, our hackathons are the perfect stage
+              to challenge yourself and push boundaries.
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={hackStyle.hacksection2}
+          ref={section2Ref}
+          smooth={true}
+          duration={600}
+        >
+          <div>
+            <Fade bottom duration={2000} distance="50px">
+              <div className={hackStyle.sec2heading}>Why Join?</div>
+            </Fade>
+          </div>
+
+          <div class="col-11  mx-auto">
+            <div class="row align-items-stretch">
+              <div class="col-md-6 col-lg-3  my-2">
+                <Fade duration={1000} delay={500}>
+                  <div className={hackStyle.sec2box}>
+                    <div className={hackStyle.sec2boxTitle}>Networking</div>
+                    <div className={hackStyle.sec2boxpera}>
+                      Engage with a vibrant community of blockchain enthusiasts,
+                      industry veterans, and budding innovators.
+                    </div>
+                  </div>
+                </Fade>
+              </div>
+              <div class="col-md-6 col-lg-3 my-2">
+                <Fade duration={1000} delay={700}>
+                  <div className={hackStyle.sec2box}>
+                    <div className={hackStyle.sec2boxTitle}>Learn</div>
+                    <div className={hackStyle.sec2boxpera}>
+                      Benefit from expert-led sessions, workshops, and hands-on
+                      coding challenges.
+                    </div>
+                  </div>
+                </Fade>
+              </div>
+              <div class="col-md-6 col-lg-3 my-2">
+                <Fade duration={1000} delay={900}>
+                  <div className={hackStyle.sec2box}>
+                    <div className={hackStyle.sec2boxTitle}>Innovate</div>
+                    <div className={hackStyle.sec2boxpera}>
+                      Bring your groundbreaking ideas to life and contribute to
+                      real-world blockchain solutions.
+                    </div>
+                  </div>
+                </Fade>
+              </div>
+
+              <div class="col-md-6 col-lg-3 my-2">
+                <Fade duration={1000} delay={1200}>
+                  <div className={hackStyle.sec2box}>
+                    <div className={hackStyle.sec2boxTitle}>Rewards</div>
+                    <div className={hackStyle.sec2boxpera}>
+                      Win exciting prizes, gain recognition, and potentially
+                      land collaborations or partnerships!
+                    </div>
+                  </div>
+                </Fade>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={hackStyle.hacksection3}
+          ref={section3Ref}
+          smooth={true}
+          duration={600}
+        >
+          <div>
+            <Fade bottom duration={2000} distance="5px">
+              <div className={hackStyle.sec3heading}>Upcoming Hackathons</div>
+            </Fade>
+          </div>
+
+          <div style={{ margin: "50px 0px" }}>
+            <div className={`${hackStyle["sec3box1"]} col-10`}>
+              <div className={hackStyle.sec3boxheading}>
+                Lampros Web3 Vision 2023
+              </div>
+            </div>
+            <div className={`${hackStyle["sec3box2"]} col-10`}>
+              <div className={hackStyle.sec3hackflex}>
+                <div className={hackStyle.sec3flex1}>
+                  <div className={`${hackStyle["s3date"]} col-3`}>Date:</div>
+                  <input type="text" id="username" name="username" />
+                </div>
+                <div className={hackStyle.sec3flex1}>
+                  <div className={`${hackStyle["s3date"]} col-3`}>Theme:</div>
+                  <input type="text" id="username" name="username" />
+                </div>
+                <div>
+                  <button id={hackStyle.button7} className={hackStyle.sec3btn}>
+                    <div id={hackStyle.dubArrowRegister}>
+                      <Image src={arrow} />
+                    </div>
+                    Register Now!
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={`${hackStyle["hacksection4"]} col-9  mx-auto`}
+          style={{ padding: "50px 0px" }}
+        >
+          <div className={hackStyle.s4heading}>Hackathon Resources</div>
+
+          <div className="sec4-hack-res">
+            <div className={hackStyle.sec4flex1}>
+              <div className={`${hackStyle["s4Res"]} col-4`}>Workshops</div>
+
+              <div className={`${hackStyle["s4pera"]} col-6`}>
+                Pre-hackathon webinars and tutorials to gear up your skills.
+              </div>
+            </div>
+            <div className={hackStyle.sec4flex1}>
+              <div className={`${hackStyle["s4Res"]} col-4`}>Mentorship</div>
+
+              <div className={`${hackStyle["s4pera"]} col-6`}>
+                On-hand expert guidance throughout the event.
+              </div>
+            </div>
+            <div className={hackStyle.sec4flex1}>
+              <div className={`${hackStyle["s4Res"]} col-4`}>Toolkits</div>
+
+              <div className={`${hackStyle["s4pera"]} col-6`}>
+                Access to premium tools, APIs, and datasets to aid your
+                development process.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default hackathonSection;
