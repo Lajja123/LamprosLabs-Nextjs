b5240aed588f822ae01e48bc5278135240d15473
Lajja123 <lajja.lamprostech@gmail.com>
2024-02-03 15:12:41 +0530

    modufy the og:tags

--- src/app/dao-roadmap/page.js
+++ src/app/dao-roadmap/page.js
@@ -12,16 +12,18 @@ export const metadata = {
     url: "https://lampros-labs-nextjs-new.vercel.app/dao-roadmap",
     type: "website",
     siteName: "Next.js",
+    image:
+      "https://www.kasandbox.org/programming-images/avatars/old-spice-man.png",
     images: [
       {
         url: "https://assets-global.website-files.com/63e140bbb85d2537a191962d/63e140bbb85d25def591973b_image-hero-finantech-x-webflow-template.svg",
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 630,
       },
       {
         url: "https://assets-global.website-files.com/63e140bbb85d2537a191962d/63e140bbb85d25def591973b_image-hero-finantech-x-webflow-template.svg",
-        width: 1800,
-        height: 1600,
+        width: 1200,
+        height: 630,
         alt: "My custom alt",
       },
     ],
--- src/app/page.js
+++ src/app/page.js
@@ -6,34 +6,35 @@ export default function Home() {
   return (
     <div>
       <div className={homeStyle.lmplabHomepageMainBg}>
-        <Head>
-          <title>Home Page</title>
-          <meta name="description" content="Home Page Description..." />
-          <meta name="keywords" content="react, meta tags, seo" />
-          <meta name="author" content="Your Name" />
-          <meta property="og:title" content="Home Page" />
-          <meta property="og:description" content="Home Page Description..." />
-          <meta
-            property="og:image"
-            content="https://lampros-labs-nextjs-new.vercel.app/"
-          />
-          <meta
-            property="og:url"
-            content="https://lampros-labs-nextjs-new.vercel.app/"
-          />
-          <meta name="twitter:title" content="My Page Title" />
-          <meta
-            name="twitter:description"
-            content="This is a description of my page"
-          />
-          <meta
-            name="twitter:image"
-            content="https://lampros-labs-nextjs-new.vercel.app/"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
         <HomeSection />
       </div>
     </div>
   );
+}
+export const metadata = {
+  title: "Home Page",
+  description: "Home Page Description...",
+  openGraph: {
+    title: "Home Page",
+    description: "Dao Page Description...",
+    url: "https://lampros-labs-nextjs-new.vercel.app/",
+    type: "website",
+    siteName: "Next.js",
+    image:
+      "https://www.kasandbox.org/programming-images/avatars/old-spice-man.png",
+    images: [
+      {
+        url: "https://assets-global.website-files.com/63e140bbb85d2537a191962d/63e140bbb85d25def591973b_image-hero-finantech-x-webflow-template.svg",
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: "https://assets-global.website-files.com/63e140bbb85d2537a191962d/63e140bbb85d25def591973b_image-hero-finantech-x-webflow-template.svg",
+        width: 1200,
+        height: 630,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+  },
 }
