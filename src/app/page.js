113b5ed7c85185cc0b94efd331307d0c2b0f862d
Lajja123 <lajja.lamprostech@gmail.com>
2024-02-05 18:08:55 +0530

    add the next/seo

--- src/app/page.js
+++ src/app/page.js
@@ -6,39 +6,39 @@ import { NextSeo } from "next-seo";
 export default function Home() {
   return (
     <div>
-      <NextSeo
-        title="Home Page Title"
-        description="Home page description of the page"
-      />
       <div className={homeStyle.lmplabHomepageMainBg}>
+        <NextSeo
+          title="Home Page Title"
+          description="Home page description of the page"
+        />
         <HomeSection />
       </div>
     </div>
   );
 }
-export const metadata = {
-  title: "Home Page",
-  description: "Home Page Description...",
-  openGraph: {
-    title: "Home Page",
-    description: "Home Page Description...",
-    url: "https://lampros-labs-nextjs-new.vercel.app/",
-    siteName: "Next.js",
+// export const metadata = {
+//   title: "Home Page",
+//   description: "Home Page Description...",
+//   openGraph: {
+//     title: "Home Page",
+//     description: "Home Page Description...",
+//     url: "https://lampros-labs-nextjs-new.vercel.app/",
+//     siteName: "Next.js",
 
-    images: [
-      {
-        url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
+//     images: [
+//       {
+//         url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+//         width: 800,
+//         height: 600,
+//       },
+//       {
+//         url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+//         width: 1800,
+//         height: 1600,
+//         alt: "My custom alt",
+//       },
+//     ],
+//     locale: "en_US",
+//     type: "website",
+//   },
+// };
