cba53bb73ac73053565bb94ee1c6f77e9ffecf0d
Lajja123 <lajja.lamprostech@gmail.com>
2024-11-13 10:33:02 +0530

    Resolved rendering error

--- src/app/components/governance/GovernanceSection.js
+++ src/app/components/governance/GovernanceSection.js
@@ -1,16 +1,14 @@
 import React from "react";
-import governance from "@/app/assets/images/governance.png"
-import styles from "../../styles/governance.module.scss"
+import governance from "@/app/assets/images/governance.png";
+import styles from "../../styles/governance.module.scss";
 import Image from "next/image";
-import { Fade } from "react-reveal";
 
-
-const Governancesection = () => {
+function governanceSection() {
   return (
     <div className={styles.aboutUsMainBg}>
       <div
         className={styles.aboutUsMain}
-        ref={section1Ref}
+        // ref={section1Ref}
         smooth={true}
         duration={600}
       >
@@ -23,21 +21,21 @@ const Governancesection = () => {
             <div className={`${styles["aboutUsTitle"]} pb-3`}>About Us</div>
 
             {/* <Fade duration={1500} delay={100}> */}
-              <div className={styles.aboutSec1Box}>
-                <p className={styles.aboutUsSection1CardDesc}>
-                  In the vibrant world of blockchain, Lampros DAO stands as a
-                  beacon, illuminating the path for innovators, dreamers, and
-                  builders. Founded with a profound vision to seamlessly merge
-                  blockchain technology with mainstream applications, we've
-                  steadily grown into a robust community hub.
-                </p>
-              </div>
+            <div className={styles.aboutSec1Box}>
+              <p className={styles.aboutUsSection1CardDesc}>
+                In the vibrant world of blockchain, Lampros DAO stands as a
+                beacon, illuminating the path for innovators, dreamers, and
+                builders. Founded with a profound vision to seamlessly merge
+                blockchain technology with mainstream applications, we've
+                steadily grown into a robust community hub.
+              </p>
+            </div>
             {/* </Fade> */}
           </div>
         </section>
       </div>
     </div>
   );
-};
+}
 
-export default Governancesection;
+export default governanceSection;
