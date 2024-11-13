f7ac3fb6a1023c7850ceadb50e171c29311b3e19
Lajja123 <lajja.lamprostech@gmail.com>
2024-11-13 10:33:02 +0530

    Resolved rendering error

--- src/app/governance/page.js
+++ src/app/governance/page.js
@@ -1,33 +1,12 @@
-import Governancesection from "../components/governance/governancesection";
+import React from "react";
+import GovernanceSection from "../components/governance/governanceSection";
 
-export default function governancePage() {
+function governanceSection() {
   return (
     <div>
-      <p>hgfdfg</p>
-      <Governancesection />
+      <GovernanceSection />
     </div>
   );
 }
-export const metadata = {
-  title: "Governance",
-  openGraph: {
-    title: "Governance",
-    url: "https://lampros-labs-nextjs-new.vercel.app/governance",
-    siteName: "LamprosDAO",
-    // images: [
-    //   {
-    //     url: "https://uniswap.org/images/twitter-card.jpg",
-    //     width: 800,
-    //     height: 600,
-    //   },
-    //   {
-    //     url: "https://uniswap.org/images/twitter-card.jpg",
-    //     width: 1800,
-    //     height: 1600,
-    //     alt: "My custom alt",
-    //   },
-    // ],
-    locale: "en_US",
-    type: "website",
-  },
-};
+
+export default governanceSection;
